f853588d614d1fea6411624d2c543c028e527ba5
Seunghoon Baik <j00hoon1101@gmail.com>
2023-05-03 21:37:50 -0400

    String to Morse code

--- PythonProject/ProjectConvertMorseCode/main.py
+++ PythonProject/ProjectConvertMorseCode/main.py
@@ -25,13 +25,38 @@ MORSE_DICT = {'A':'.-', 'B':'-...',
             'x': '-..-', 'y': '-.--', 'z': '--..'
             }
 
+morseRes = []
+stringRes = []
+strToMorse = True
+
+while strToMorse:
+    print("Choose the number.")
+    print("1. String to Morse code.")
+    print("2. See what I input so far.")
+    print("3. Exit.")
+    menu = int(input("-> "))
 
 
-originalInput = input('What?\n')
-morseInput = ""
+    if menu == 1:
+        originalInput = input('What?\n')
+        morseInput = ""
 
-for i in range(0, len(originalInput)):
-    morseInput += MORSE_DICT.get(originalInput[i])
+        for i in range(0, len(originalInput)):
+            morseInput += MORSE_DICT.get(originalInput[i])
 
-print(f"Original input : {originalInput}\nMorse input : {morseInput}")
+        print(f"Original input : {originalInput}\nMorse input : {morseInput}\n")
+        stringRes.append(originalInput)
+        morseRes.append(morseInput)
+    elif menu == 2:
+        print("--- String input ---")
+        for i in range(0, len(stringRes)):
+            print(stringRes[i])
+        print()
+        for i in range(0, len(morseRes)):
+            print(morseRes[i])
+        print("\n")
+    elif menu == 3:
+        strToMorse = False
+    else:
+        print("Wrong input.\nPlease input again.\n")
 
